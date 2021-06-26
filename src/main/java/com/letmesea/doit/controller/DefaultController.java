4710ea4f1b50419c450dc9a31a6f7ec4ff86eaf9
Letmesea <LIANGZHONGHAI3@ceair.com>
2021-06-26 22:43:09 +0800

    get data

---
+++ src/main/java/com/letmesea/doit/controller/DefaultController.java
@@ -0,0 +1,11 @@
+package com.letmesea.doit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class DefaultController {
+    @RequestMapping(value="/")
+    public String index(){
+        return "index";
+    }
+}

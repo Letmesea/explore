fa982ef52fecdad25c1a69b77eb9a66eedf2e7a2
Letmesea <LIANGZHONGHAI3@ceair.com>
2021-08-10 01:14:20 +0800

    get data

--- src/main/java/com/letmesea/doit/controller/LetmeseeController.java
+++ src/main/java/com/letmesea/doit/controller/LetmeseeController.java
@@ -2,7 +2,10 @@ package com.letmesea.doit.controller;
 
 import com.letmesea.doit.service.DealService;
 import com.letmesea.doit.utils.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +16,14 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/see")
+@Slf4j
 public class LetmeseeController {
+    private static Logger logger = Logger.getLogger(LetmeseeController.class);
     @Autowired
     private DealService dealService;
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     RedisUtils redisUtils;
     @GetMapping(value = "/ssq")
@@ -42,7 +50,14 @@ public class LetmeseeController {
 
     @GetMapping("/test")
     public String test(){
+        logger.info("端口"+port);
         return "test";
+    }
+    @GetMapping("/testlb")
+    @ResponseBody
+    public String testlb(){
+        logger.info("端口"+port);
+        return port;
     }
 
 }

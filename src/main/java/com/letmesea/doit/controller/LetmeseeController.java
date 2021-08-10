195853a5dae1d38629fc4234b3444c3707beea3d
liangzhonghai3 <liangzhonghai3@ceair.com>
2021-08-10 18:09:27 +0800

    update test

--- src/main/java/com/letmesea/doit/controller/LetmeseeController.java
+++ src/main/java/com/letmesea/doit/controller/LetmeseeController.java
@@ -1,8 +1,11 @@
 package com.letmesea.doit.controller;
 
+import com.letmesea.doit.config.DruidConfig;
 import com.letmesea.doit.service.DealService;
 import com.letmesea.doit.utils.RedisUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +17,12 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/see")
 public class LetmeseeController {
+    private static Logger logger = Logger.getLogger(LetmeseeController.class);
     @Autowired
     private DealService dealService;
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     RedisUtils redisUtils;
     @GetMapping(value = "/ssq")
@@ -39,9 +46,11 @@ public class LetmeseeController {
     public void getDlt(){
 
     }
-
+    @ResponseBody
     @GetMapping("/test")
     public String test(){
+        logger.info(1111);
+        System.out.println(port);
         return "test";
     }
 
